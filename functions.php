9d7c298338a0bfc0fa5f7f4556e330d6d3e770ac
simonho <simon@kubuntu>
2018-04-04 10:10:07 +0800

    Declare Google API object in index.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function enqueue_style_func() {
+}
+add_action('wp_enqueue_scripts', 'enqueue_style_func');
+
+function enqueue_script_func() {
+}
+add_action('wp_enqueue_scripts', 'enqueue_script_func');
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php /* Template Name: FullCalendar */?>
+
+<?php
+require_once 'vendor/autoload.php'; // install the composer libraries
+
+$gapi = new Google_Client();
+$gapi->setAuthConfig(__DIR__ . '/service-account.json');
+$gapi->setApplicationName('Google Calendar Wordpress Theme');
+$gapi->addScope(Google_Service_Calendar::CALENDAR);
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_title(); ?></title>
+    <meta http-equiv="pragma" content="no-cache" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <h1>Hello, world...</h1>
+    <?php wp_footer(); ?>
+  </body>
+ </html>
