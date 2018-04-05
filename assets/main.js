a06617e455326e7d2de2de196847e6ca83935ddf
simonho <simon@kubuntu>
2018-04-05 10:03:10 +0800

    Add FullCalendar handlers

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,57 @@
 'use strict';
 
 var $ = undefined; // jQuery shorthand
+var events = [];
 
 jQuery(document).ready(function(jquery) {
   $ = jquery;
-  $('#calendar').fullCalendar({
-  });
+  initCalendar();
 });
+
+function initCalendar() {
+  $('#calendar').fullCalendar({
+    viewRender: onViewRender,
+    defaultView: 'agendaWeek',
+    dayClick: onDayClick,
+    eventClick: onEventClick,
+    events: events,
+    editable: true
+  });
+}
+
+function refreshCalendar(newEvent) {
+  $('#calendar').fullCalendar('updateEvents', events);
+  $('#calendar').fullCalendar('renderEvent', newEvent);
+}
+
+function onViewRender(view, elem) {
+  // Get the date range will be displaying
+  var timezone = -(new Date().getTimezoneOffset() / 60);
+  var dateStart = view.intervalStart.format() + 'T00:00:00';
+  var dateEnd = view.intervalEnd.format() + 'T00:00:00';
+  console.log(dateStart + ' - ' + dateEnd);
+
+  // Create a sample event
+  events.push({
+    title: 'event1',
+    start: '2018-04-04T12:30:00',
+    end: '2018-04-04T13:00:00'
+  });
+}
+
+function onDayClick(date, jsEvent, view) {
+  // When a time clicked, just create and add a sample event to the calendar.
+  var title = 'Event' + (events.length + 1).toString();
+  var newEvent = {
+    title: title,
+    start: date.format(),
+    end: date.add(30, 'm').format()
+  };
+  events.push(newEvent);
+  refreshCalendar(newEvent);
+}
+
+function onEventClick(event, element) {
+  // When a event clicked, just remove it from calendar.
+  $('#calendar').fullCalendar('removeEvents', event._id);
+}
--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ add_action('wp_enqueue_scripts', 'enqueue_style_func');
 
 function enqueue_script_func() {
 	wp_enqueue_script('moment-js', get_template_directory_uri() . '/assets/moment.min.js', array('jquery'), '1.0', true);
+	// wp_enqueue_script('moment-timezone-js', get_template_directory_uri() . '/assets/moment-timezone.min.js', array('moment-js'), '1.0', true);
+	wp_enqueue_script('moment-timezone-js', get_template_directory_uri() . '/assets/moment-timezone-with-data.min.js', array('moment-js'), '1.0', true);
 	wp_enqueue_script('fullcalendar-js', get_template_directory_uri() . '/assets/fullcalendar.min.js', array('moment-js'), '1.0', true);
 	wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/main.js', array('fullcalendar-js'), '1.0', true);
 }
